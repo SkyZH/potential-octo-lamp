e56b51027927326cb94536fc838b6d8bf481b552
SkyZH <iSkyZH@gmail.com>
2016-01-27 10:20:47 +0800

    add code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # potential-octo-lamp
+
 Word List Generator for Python
+
+Transfer Text to Python List
+
+Data Source: [1,000 most common US English words](https://gist.github.com/deekayen/4148741)
+
+## Usage
+
+    chmod +x main.py
+    ./main.py --input wiki-100k.txt --output words.py
+    ./main.py --input wiki-100k.txt --output words.py
---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Text File to Python List')
+
+parser.add_argument('--input', help='text file path')
+parser.add_argument('--output', help='python file path')
+
+args = parser.parse_args()
+
+in_file = open(args.input, "rt")
+out_file = open(args.output, "wt")
+
+out_file.write("words = [\n")
+words = [word[:-1] for word in in_file.readlines()]
+for data in words[:-1]:
+    if not(data.startswith("#!")):
+        out_file.write("\"" + data + "\"")
+        out_file.write(",\n")
+out_file.write("\"" + words[-1] + "\"")
+out_file.write("\n]\n")
+
+in_file.close()
+out_file.close()
